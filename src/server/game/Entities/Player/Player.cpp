e765256c761a869f619e8d3dbf7f7e5e00b20567
jackhein <moustiquejunior@gmail.com>
2023-12-02 17:11:23 +0100

    mod-talent-points-quests

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -13496,25 +13496,12 @@ LootItem* Player::StoreLootItem(uint8 lootSlot, Loot* loot, InventoryResult& msg
 
 uint32 Player::CalculateTalentsPoints() const
 {
-    uint32 base_talent = GetLevel() < 10 ? 0 : GetLevel() - 9;
-
-    uint32 talentPointsForLevel = 0;
-    if (getClass() != CLASS_DEATH_KNIGHT || GetMapId() != 609)
-    {
-        talentPointsForLevel = base_talent;
-    }
-    else
-    {
-        talentPointsForLevel = GetLevel() < 56 ? 0 : GetLevel() - 55;
-        talentPointsForLevel += m_questRewardTalentCount;
-
-        if (talentPointsForLevel > base_talent)
-        {
-            talentPointsForLevel = base_talent;
-        }
-    }
-
+    auto talent_limit = static_cast<uint32>(GetLevel() < 10 ? 0 :
+            GetLevel() - 4 < sWorld->getIntConfig(CONFIG_MAX_PLAYER_LEVEL) - 9 ?
+            GetLevel() - 4 : sWorld->getIntConfig(CONFIG_MAX_PLAYER_LEVEL) - 9);
+    uint32 talentPointsForLevel = m_questRewardTalentCount < talent_limit ? m_questRewardTalentCount : talent_limit;
     talentPointsForLevel += m_extraBonusTalentCount;
+
     return uint32(talentPointsForLevel * sWorld->getRate(RATE_TALENT));
 }
 
